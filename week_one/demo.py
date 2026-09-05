dd13c0ebb83d3bb541ebeca16aeb31fdc8477714
Akhil Gupta <akhil.gupta@bluetickconsultants.com>
2026-09-05 21:54:45 +0530

    week one resources

---
+++ week_one/demo.py
@@ -0,0 +1,58 @@
+
+# data structures
+#School 
+# marks = [85, 90, 78]
+
+# #professional style
+# student = {
+#     "name": "Akhil",
+#     "marks": [85, 90, 78],
+#     "grade" : "A"
+# }
+
+# # Accessing it
+# # print(student["name"])
+
+
+# # print(sum(student["marks"]) / len(student["marks"]))
+
+
+
+# #Funcitons
+
+# #Facebook
+
+# # 1. Break your code in smaller parts
+# # 2. Impleemnti tiin fucntions
+
+# def calculate_average(scores):
+#     return sum(scores) / len(scores)
+
+
+# print(calculate_average(student["marks"]))
+
+
+
+passing_marks = []
+for mark in [85, 45, 90, 32, 78]:
+    if mark >= 50:
+        passing_marks.append(mark)
+
+print(passing_marks)
+
+
+passing_marks = [mark for mark in [85, 45, 90, 32, 78] if mark >=50]
+
+print(passing_marks)
+
+
+#calculate average
+
+
+#calcuting passing parks
+
+
+
+
+CSV file - marks, stu
+
